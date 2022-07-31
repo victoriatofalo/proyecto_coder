24d886439e7c318525d0d83ba3296405571b2f23
victoriatofalo <victoriatofalo@hotmail.com>
2022-07-31 20:18:39 -0300

    tempaltes agregados

--- coder/views.py
+++ coder/views.py
@@ -1,17 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from coder.models import Curso,Estudiante,Porfesor,Entregable
+from coder.models import Curso,Estudiante,Profesor,Entregable
 # Create your views here.
 def cursos(request):
     
     curso = Curso.objects.all()
     lista_cursos_nombres = []
-    lista_cursos_nombres.append(curso.nombre)
-    return HttpResponse(lista_cursos_nombres)
+    for curso in cursos:
+        lista_cursos_nombres.append(curso.nombre)
+        return HttpResponse(lista_cursos_nombres)
     
 
 def inicio(request):
-    return HttpResponse("Vista de inicio")
+    return render(request,"coder/index.html",{"mensaje":"La comision es la mejor"})
 
 def estudiante(request):
     return HttpResponse("Vista de estudiante")
